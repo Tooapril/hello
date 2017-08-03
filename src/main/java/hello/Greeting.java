d2df4b82e7ac0d47a329c907702ea3b39ede9aa6
Tooapril <yxq_1997@163.com>
2017-08-03 15:09:33 +0800

    Create a resource controller

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by leslie on 2017/8/3.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
